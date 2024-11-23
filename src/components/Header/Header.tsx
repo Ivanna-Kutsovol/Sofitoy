3ebac26561d27549f172da49915d44aedfdd17cf
Ivanna-Kutsovol <st6815720@stud.duikt.edu.ua>
2024-11-24 00:38:20 +0200

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import Logo from "../UI/Logo/Logo";
+import Link from "next/link";
+import Button from "../UI/Button/Button";
+import burgerImg from "/public/imgHeader/burgerImg.svg";
+import MobileMenu from "../MobileMenu/MobileMenu";
+import styles from "./Header.module.scss";
+import { useEffect, useRef, useState } from "react";
+
+const Header = () => {
+    const [burgerMenu, setMenu] = useState(false);
+    const openBurgerMenu = () => setMenu(!burgerMenu);
+
+    const [showHeader, setShowHeader] = useState(true);
+    const [headerHeight, setHeaderHeight] = useState(0);
+    const headerRef = useRef<HTMLDivElement | null>(null); 
+
+    let lastScroll = 0;
+
+    useEffect(() => {
+        const updateHeaderHeight = () => {
+            if (headerRef.current) {
+                setHeaderHeight(headerRef.current.offsetHeight); 
+            }
+        };
+
+        updateHeaderHeight(); 
+        window.addEventListener("resize", updateHeaderHeight);
+
+        const handleScroll = () => {
+            const currentScroll = window.scrollY;
+
+            if (currentScroll > lastScroll && currentScroll > 100) {
+                setShowHeader(false);
+            } else {
+                setShowHeader(true); 
+            }
+
+            lastScroll = currentScroll;
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("resize", updateHeaderHeight);
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <>
+            <div style={{ height: `${headerHeight}px`, display:'flex', justifyContent:'center'}}>
+            <header
+                ref={headerRef} // Привязываем ref к хедеру
+                className={`${styles.header} ${!showHeader ? styles.header__hidden : ""}`}
+            >
+            <div className={styles.header__head}>
+                <Link href="/">
+                    <Logo></Logo>
+                </Link>
+                
+                <nav className={styles.mainNav}>
+                    <ul className={styles.mainNav__list}>
+                        <li className={styles.mainNav__item}>
+                            <Link href="#about-us" scroll={true} className={styles.mainNav__link}>About us</Link>
+                        </li>
+                        <li className={styles.mainNav__item}>
+                            <Link href="#home-catalogy" scroll={true} className={styles.mainNav__link}>Catalogy</Link>
+                        </li>
+                        <li className={styles.mainNav__item}>
+                            <Link href="#feedback" scroll={true} className={styles.mainNav__link}>Feedback</Link>
+                        </li>
+                        <li className={styles.mainNav__item}>
+                            <Link href="#specials-conditions" scroll={true} className={styles.mainNav__link}>For Partrens</Link>
+                        </li>
+                        <li className={styles.mainNav__item}>
+                            <Link href="#order-call" scroll={true} className={styles.mainNav__link}>Contacts</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Button
+                    className={styles.burgerMenu}
+                    burgerImg={burgerImg}
+                    alt="burger menu"
+                    onClick={openBurgerMenu}
+                    type="button"
+                />
+            </div>
+                <MobileMenu burgerMenu={burgerMenu} openBurgerMenu={openBurgerMenu}></MobileMenu>
+        </header>
+        </div>
+        </>
+    )
+}
+export default Header;
---
+++ src/components/forMain/InitialSlide/Sliders/Sliderpage.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import styles from "./Sliderpage.module.scss";
+import ArrowLeftActive from "/public/ImgSliders/arrowLeftActive.svg";
+import ArrowLeft from "/public/ImgSliders/arrowLeft.svg";
+import ArrowRightActive from "/public/ImgSliders/arrowRightActive.svg";
+import ArrowRight from "/public/ImgSliders/arrowRight.svg";
+
+const InitialSliders = (data: any) => {
+  const dataArr = data.data[0];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isActiveLeft, setIsActiveLeft] = useState(false);
+  const [isActiveRight, setIsActiveRight] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false); 
+  const [nextIndex, setNextIndex] = useState(0);
+  const [direction, setDirection] = useState('right')
+
+  useEffect(() => {
+        if (isAnimating) {
+          const timeout = setTimeout(() => {
+            setCurrentIndex(nextIndex); 
+            setIsAnimating(false); 
+            setDirection('right')
+          }, 2000);
+    
+          return () => clearTimeout(timeout); 
+        }
+      }, [isAnimating, nextIndex, currentIndex, direction]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(!isAnimating) {
+        setNextIndex((currentIndex) => (currentIndex + 1) % dataArr.length);
+        setIsAnimating(true); 
+      }
+    }, 9000); 
+
+    return () => clearInterval(interval); 
+  }, [currentIndex, dataArr.length, isAnimating]);
+  
+  const handleClickLeft = () => {
+    if(!isAnimating){
+      setIsActiveLeft(true);
+      setIsActiveRight(false);
+      setDirection('left')
+      const newIndex = currentIndex === 0 ? dataArr.length - 1 : currentIndex - 1;
+      setNextIndex(newIndex); 
+      setIsAnimating(true);
+    }    
+  };
+
+  const handleClickRight = () => {
+    if(!isAnimating){
+      setIsActiveLeft(false);
+      setIsActiveRight(true);
+      setDirection('right')
+      const newIndex = currentIndex === 0 ? dataArr.length - 1 : currentIndex - 1;
+      setNextIndex(newIndex); 
+      setIsAnimating(true);
+    }   
+  };
+
+  return (
+    <div className={styles.slides}>
+      <section className={styles.slides__container}>
+        <a className={`${styles.slides__backgroundSlideSmall}`} 
+         style={{
+          backgroundImage: `url('/ImgSliders/${dataArr[nextIndex].ImgSlideSmall}')`,
+          backgroundRepeat: 'no-repeat'
+        }}
+         >
+          <Image
+            unoptimized 
+            src={`/ImgSliders/${dataArr[currentIndex].ImgSlideSmall}`}
+            alt="slider"
+            width={200}
+            height={600}
+            priority={true}
+            className={isAnimating ? 
+              direction === 'right' 
+              ? styles.slides__animatingSmall : styles.slides__animatingSmallLeft : ''} 
+          />
+        </a>
+
+        <section className={styles.slides__info}>
+          <h1 className={styles.slides__topic}>Create while playing!</h1>
+          <p className={styles.slides__text}>
+          Discover a world of toys that develop your child&apos;s fine motor skills and creativity.
+          </p> 
+          <section className={styles.slides__arrow}>
+            <button
+              className={isActiveLeft ? styles.slides__ArrowLeftActive : styles.slides__ArrowLeft}
+              onClick={handleClickLeft} 
+              disabled={isAnimating}
+            >
+              <Image
+                src={isActiveLeft ? ArrowLeftActive : ArrowLeft}
+                alt="arrow left"
+                width={90}
+                height={90}
+              />
+            </button>
+
+            <button
+              className={isActiveRight ? styles.slides__ArrowRightActive : styles.slides__ArrowRight}
+              onClick={handleClickRight}
+              disabled={isAnimating}
+              >
+              <Image
+                src={isActiveRight ? ArrowRight : ArrowRightActive}
+                alt="arrow right"
+                width={90}
+                height={90}
+              />
+            </button>
+          </section>
+        </section>
+
+        <a className={`${styles.slides__backgroundImgSlide} `}
+        style={{
+          backgroundImage: `url('/ImgSliders/${dataArr[nextIndex].ImgSlide}')`,
+          backgroundRepeat: 'no-repeat'
+          
+          }}
+        >
+          <Image
+            unoptimized 
+            src={`/ImgSliders/${dataArr[currentIndex].ImgSlide}`}
+            alt="slider"
+            width={470}
+            height={600}
+            priority={true}
+            className={isAnimating ?
+              direction === 'right'
+              ? styles.slides__animatingLarge : styles.slides__animatingLargeLeft : ''
+            }  
+          />
+        </a>
+      </section>
+    </div>
+  );
+}
+
+export default InitialSliders;
